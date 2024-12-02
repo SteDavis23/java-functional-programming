3c44c92d701c9d26ad524a5d01fee429cb0a639f
Stephen Davis <stephen.davis@gong.io>
2024-12-02 19:36:26 +0000

    summing list of objects into 1 value using mapToDouble

--- src/chapter3/Chapter3.java
+++ src/chapter3/Chapter3.java
@@ -4,18 +4,29 @@ import java.util.List;
 import java.util.Optional;
 
 public class Chapter3 {
+    List<Grade> grades = List.of(new Grade("Stephen", 80.0), new Grade("John", 32.0), new Grade("Mark", 79.9));
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         Optional<String> firstFailedStudent = new Chapter3().optionalExample();
         System.out.println("Somebody who failed the exam is: " + firstFailedStudent.orElse("Nobody failed the exam!"));
+
+        double totalMarks = new Chapter3().calculateTotalMarks();
+        System.out.println("Total collective marks achieved by everyone: " + totalMarks);
     }
 
     private Optional<String> optionalExample() {
-        List<Grade> grades = List.of(new Grade("Stephen", 80.0), new Grade("John", 32.0), new Grade("Mark", 79.9));
         Optional<Grade> optionalFirstFailedGrade = grades.stream()
                 .filter(grade -> grade.getScore() < 40)
                 .findFirst();
         return optionalFirstFailedGrade.map(Grade::getName);
+    }
+
+    private double calculateTotalMarks() {
+        return grades
+                .stream()
+                .mapToDouble(Grade::getScore)
+                .sum();
     }
 
     private class Grade {
