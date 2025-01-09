f31d6dffccb75be60bcee7e8379565bd54639731
Stephen Davis <stephen.davis@gong.io>
2025-01-09 17:07:07 +0000

    flat map demo to collect nested employees

--- src/chapter4TransformingData/TransformingData.java
+++ src/chapter4TransformingData/TransformingData.java
@@ -3,6 +3,7 @@ package src.chapter4TransformingData;
 import src.utilities.Employee;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TransformingData {
 
@@ -11,6 +12,7 @@ public class TransformingData {
     public static void main(String[] args) {
         double totalNumberOfEmployees = new TransformingData().findAverageNumberOfReportees();
         System.out.println("Average number of employees: " + totalNumberOfEmployees);
+        System.out.println("All employees count: " + new TransformingData().retrieveAllEmployeesIgnoringHierarchy().size());
     }
 
     public double findAverageNumberOfReportees() {
@@ -19,5 +21,15 @@ public class TransformingData {
                 .mapToDouble(reportees -> reportees.size())
                 .average()
                 .orElse(0.0);
+    }
+
+    private List<Employee> reportees;
+    private String name;
+    private int age;
+
+    public List<Employee> retrieveAllEmployeesIgnoringHierarchy() {
+        return employees.stream()
+                .flatMap(employee -> employee.getReportees().stream())
+                .collect(Collectors.toList());
     }
 }
