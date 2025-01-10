897461121676adbd221813611db0af01313a5920
Stephen Davis <stephen.davis@gong.io>
2025-01-10 17:48:03 +0000

    example using partitioningBy of collect method

--- src/chapter4TransformingData/TransformingData.java
+++ src/chapter4TransformingData/TransformingData.java
@@ -3,17 +3,30 @@ package src.chapter4TransformingData;
 import src.utilities.Employee;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.partitioningBy;
 
 public class TransformingData {
 
     public static List<Employee> employees = new Employee().generateSomeEmployees();
+    private static List<Integer> ages = List.of(1,32,14,22,19);
 
     public static void main(String[] args) {
-        double totalNumberOfEmployees = new TransformingData().findAverageNumberOfReportees();
-        System.out.println("Average number of employees: " + totalNumberOfEmployees);
-        System.out.println("Nested employees' names: " + new TransformingData().retrieveAllEmployeesIgnoringHierarchy());
-    }
+//        double totalNumberOfEmployees = new TransformingData().findAverageNumberOfReportees();
+//        System.out.println("Average number of employees: " + totalNumberOfEmployees);
+//        System.out.println("Nested employees' names: " + new TransformingData().retrieveAllEmployeesIgnoringHierarchy());
+         Map<Boolean, List<Integer>> partitionedAges = partitionByAgeGreaterThanFifteen();
+         Set<Map.Entry<Boolean, List<Integer>>> mapEntries = partitionedAges.entrySet();
+        System.out.println("Partitioned ages around 15 ");
+         mapEntries.forEach(System.out::println);
+         System.out.println("Ages less than or equal to fifteen: ");
+         retrieveAgesLessOrEqualToFifteen(partitionedAges).forEach(System.out::println);
+        System.out.println("Ages greater than fifteen: ");
+         retrieveAgesGreaterThanFifteen(partitionedAges).forEach(System.out::println);
+        }
 
     public double findAverageNumberOfReportees() {
         return employees.stream()
@@ -23,14 +36,18 @@ public class TransformingData {
                 .orElse(0.0);
     }
 
-    private List<Employee> reportees;
-    private String name;
-    private int age;
-
-    public List<String> retrieveAllEmployeesIgnoringHierarchy() {
-        return employees.stream()
-                .flatMap(employee -> employee.getReportees().stream())
-                .map(Employee::getName)
-                .collect(Collectors.toList());
+    public static Map<Boolean, List<Integer>> partitionByAgeGreaterThanFifteen() {
+        return ages.stream()
+                .collect(partitioningBy(age -> age > 15));
     }
+
+    public static List<Integer> retrieveAgesLessOrEqualToFifteen(Map<Boolean, List<Integer>> partitionedAges) {
+        return partitionedAges.get(false);
+    }
+
+    public static List<Integer> retrieveAgesGreaterThanFifteen(Map<Boolean, List<Integer>> partitionedAges) {
+        return partitionedAges.get(true);
+    }
+
+
 }
