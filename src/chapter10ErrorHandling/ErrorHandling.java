3767994fd9816ae68a180c7185842e7268633498
Stephen Davis <stephen.davis@gong.io>
2025-02-13 17:02:09 +0000

    feature(123): demonstrating losing custom exception details when try catching

--- src/chapter10ErrorHandling/ErrorHandling.java
+++ src/chapter10ErrorHandling/ErrorHandling.java
@@ -16,9 +16,17 @@ public class ErrorHandling {
     public static void main(String[] args) {
         List<String> studentNamesToFindGrade = List.of("John", "Michael", "Stephen", "Joe");
         List<Double> studentGradeScores = studentNamesToFindGrade.stream()
-                .mapToDouble(ErrorHandling::retrieveStudentGrade)
+                .mapToDouble(studentName -> {
+                        try {
+                            return retrieveStudentGrade(studentName);
+                        } catch (Exception exception) {
+                            System.out.printf("Exception caught trying to find student's grade for student %s%n", studentName);
+                            return 0.0;
+                        }
+                })
                 .boxed()
                 .collect(Collectors.toList());
+        studentGradeScores.forEach(System.out::println);
     }
 
     public static Double retrieveStudentGrade(String studentName) {
